f7dc63aa2ab320d86fe792edb55dda8d5204f218
nerull7 <github@nerull7.info>
2013-11-22 21:19:27 +0100

    Added missing ADVANCED_REBOOT in settings options for advanced reboot

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4423,6 +4423,11 @@ public final class Settings {
          */
         public static final String PAYMENT_SERVICE_SEARCH_URI = "payment_service_search_uri";
 
+        /** Whether to include options in power menu for rebooting into recovery or bootloader
+         * @hide
+         */
+        public static final String ADVANCED_REBOOT = "advanced_reboot";
+
         /**
          * This are the settings to be backed up.
          *
@@ -4471,7 +4476,8 @@ public final class Settings {
             MOUNT_UMS_AUTOSTART,
             MOUNT_UMS_PROMPT,
             MOUNT_UMS_NOTIFY_ENABLED,
-            UI_NIGHT_MODE
+            UI_NIGHT_MODE,
+            ADVANCED_REBOOT
         };
 
         /**
