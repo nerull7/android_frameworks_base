64b5e6cf034c5c9aaa1663f6998d1b7dff4b26e7
nerull7 <github@nerull7.info>
2014-02-25 12:59:16 +0100

    Revert "Frameworks AppOps: fix allow/reject counting"
    This reverts commit bb50d9ff7e8827e5a1346391f4cbf727740ff9c3.

--- services/java/com/android/server/AppOpsService.java
+++ services/java/com/android/server/AppOpsService.java
@@ -588,14 +588,12 @@ public class AppOpsService extends IAppOpsService.Stub {
                 if (DEBUG) Log.d(TAG, "noteOperation: reject #" + op.mode + " for code "
                         + switchCode + " (" + code + ") uid " + uid + " package " + packageName);
                 op.rejectTime = System.currentTimeMillis();
-                op.ignoredCount++;
                 return switchOp.mode;
             }
             if (DEBUG) Log.d(TAG, "noteOperation: allowing code " + code + " uid " + uid
                     + " package " + packageName);
             op.time = System.currentTimeMillis();
             op.rejectTime = 0;
-            op.allowedCount++;
             return AppOpsManager.MODE_ALLOWED;
         }
     }
@@ -619,7 +617,6 @@ public class AppOpsService extends IAppOpsService.Stub {
                 if (DEBUG) Log.d(TAG, "startOperation: reject #" + op.mode + " for code "
                         + switchCode + " (" + code + ") uid " + uid + " package " + packageName);
                 op.rejectTime = System.currentTimeMillis();
-                op.ignoredCount++;
                 return switchOp.mode;
             }
             if (DEBUG) Log.d(TAG, "startOperation: allowing code " + code + " uid " + uid
@@ -627,7 +624,6 @@ public class AppOpsService extends IAppOpsService.Stub {
             if (op.nesting == 0) {
                 op.time = System.currentTimeMillis();
                 op.rejectTime = 0;
-                op.allowedCount++;
                 op.duration = -1;
             }
             op.nesting++;
